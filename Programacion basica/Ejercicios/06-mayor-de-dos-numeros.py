f53d5d6ea9c4f93c52640697863c9d7ae704fe2a
Jose Jimenez <jimenezhernandezjosemanuel30@gmail.com>
2024-08-17 13:16:59 -0600

    Se modifico el problema 6

---
+++ Programacion basica/Ejercicios/06-mayor-de-dos-numeros.py
@@ -0,0 +1,13 @@
+def maximo(a,b):
+    if a>b:
+        return a
+    else:
+        return b
+
+def run():
+    a = int(input("Introduce el primer numero: "))
+    b = int(input("Introduce el segundo numero: "))
+    print(f"El numero mayor es: {maximo(a,b)} ")
+
+if __name__ == '__main__':
+    run()
